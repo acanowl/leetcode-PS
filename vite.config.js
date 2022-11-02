4fc5b091945df8811133678dec3cfbc95ab9c54c
徐仔 <kami.x1g@163.com>
2022-11-03 00:41:57 +0800

    ✨ feat: 22/11/3 add vitest

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import AutoImport from "unplugin-auto-import/vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    AutoImport({
+      imports: ["vitest"]
+    }),
+  ],
+  test: {
+    include: ['./project/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+  }
+})
