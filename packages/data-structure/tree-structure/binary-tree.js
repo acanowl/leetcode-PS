f5098d82174278a9c8baca34e0c3b51955f9414a
徐仔 <kami.x1g@163.com>
2022-11-04 19:14:00 +0800

    ✨ feat: 22/11/4 - 4
    初始binaryTree

---
+++ packages/data-structure/tree-structure/binary-tree.js
@@ -0,0 +1,7 @@
+import { BinaryTreeNode } from "./config";
+
+export class BinaryTree {
+  constructor() {
+    this.root = null
+  }
+}
---
+++ packages/data-structure/tree-structure/config.js
@@ -0,0 +1,14 @@
+/**
+ * @description: 创建二叉树节点类
+ * @param {*} val 节点值
+ * @param {*} left 左节点值
+ * @param {*} right 右节点值
+ * @return {*}
+ */
+export class BinaryTreeNode {
+  constructor(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+  }
+}
