062783f0bffcc076db1235f527562390a914e72e
徐仔 <kami.x1g@163.com>
2022-11-08 08:46:53 +0800

    ✨ feat: 22/11/8
    新增 数组转换二叉树 方法

--- packages/data-structure/tree-structure/config.js
+++ packages/data-structure/tree-structure/config.js
@@ -6,9 +6,48 @@
  * @return {*}
  */
 export class BinaryTreeNode {
-  constructor(val, left, right) {
-    this.val = val === undefined ? 0 : val
-    this.left = left === undefined ? null : left
-    this.right = right === undefined ? null : right
-  }
+	constructor(val, left, right) {
+		this.val = val === undefined ? 0 : val
+		this.left = left === undefined ? null : left
+		this.right = right === undefined ? null : right
+	}
 }
+
+/**
+ * 数组转换成二叉树
+ * @param {*} arr
+ * @returns
+ */
+export const createBinaryTree = arr => {
+	if (arr.length === 0 || !Array.isArray(arr)) return null
+
+	let root = new BinaryTreeNode(arr.shift())
+	let nodeQueue = [root]
+
+	while (arr.length) {
+		const cur = nodeQueue.shift()
+
+		if (!arr.length) break
+
+		const left = arr.shift()
+		if (left) {
+			const leftNode = new BinaryTreeNode(left)
+
+			cur.left = leftNode
+			nodeQueue.push(leftNode)
+		}
+
+		if (!arr.length) break
+
+		const right = arr.shift()
+		if (right) {
+			const rightNode = new BinaryTreeNode(right)
+
+			cur.right = rightNode
+			nodeQueue.push(rightNode)
+		}
+	}
+	return root
+}
+
+createBinaryTree([1, 2, 3, null, 5, 6, 7, 8, 9])
