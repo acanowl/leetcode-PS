e44692adf2fe6a10c4af3cc590c5abd50e750eb3
徐仔 <kami.x1g@163.com>
2022-11-25 23:44:24 +0800

    ✨ feat: 22/11/25 新增生成脚本

--- mkdirs.js
+++ mkdirs.js
@@ -1,10 +1,30 @@
-import { mkdirFile } from '@ao/utils'
+import { mkdirFile, rewriteFile } from '@ao/utils'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { resolve } from 'path'
 
 const argv = yargs(hideBin(process.argv)).argv
-const { name, _: [path] } = argv
+const { exec, _ } = argv
+const [filename] = _
 
-mkdirFile(`${name}${path}`)
+/**
+ * 生成算法题集md模板
+ */
+const createProblemSet = () => {
+  const pathname = resolve(`./project/collection/${filename}`)
+  const tempname = resolve('./project/template/index.md')
+  // 创建文件夹
+  mkdirFile(pathname)
+  // 生成文件
+  rewriteFile({
+    pathname: `${pathname}\\index.md`,
+    tempname: tempname,
+    format: res => {
+      const psname = filename.slice(filename.indexOf('.') + 1, filename.length)
+      return res.replace('##', `## ${psname}`)
+    }
+  })
+}
 
-// TODO 生成文件
+const creatorFnMap = { createProblemSet }
+creatorFnMap[exec] && creatorFnMap[exec]()
--- packages/utils/file.js
+++ packages/utils/file.js
@@ -1,36 +1,52 @@
 // 动态新建文件夹的方法
-import { mkdirSync, statSync } from 'fs'
-import path, { dirname } from 'path'
+import { mkdirSync, statSync, readFile, writeFile } from 'fs'
+import { dirname, isAbsolute, join, relative, sep, resolve } from 'path'
 import { fileURLToPath } from 'url'
-let __dirname = dirname(fileURLToPath(import.meta.url))
-__dirname = __dirname.slice(0, __dirname.indexOf('packages'))
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+/**
+ * 创建文件夹
+ * @param {*} pathname
+ */
 export const mkdirFile = pathname => {
   // 需要判断是否是绝对路径（避免不必要的 bug）
-  pathname = path.isAbsolute(pathname)
-    ? pathname
-    : path.join(__dirname, pathname)
-
+  pathname = isAbsolute(pathname) ? pathname : join(__dirname, pathname)
   // 获取相对路径
-  pathname = path.relative(__dirname, pathname)
+  pathname = relative(__dirname, pathname)
 
   // path.sep 避免平台差异带来的 bug
-  const floders = pathname.split(path.sep)
+  const floders = pathname.split(sep)
 
   let pre = '' // 最终用来拼合的路径
   floders.forEach(floder => {
     try {
       // 没有异常，文件已经创建，提示用户该文件已经创建
-      statSync(path.join(__dirname, pre, floder))
+      statSync(join(__dirname, pre, floder))
     } catch (err) {
       // 抛出异常，文件不存在则创建文件
       try {
         // 避免父文件还没有创建的时候，先创建子文件所出现的意外 bug，这里选择同步创建文件
-        mkdirSync(path.join(__dirname, pre, floder))
+        mkdirSync(join(__dirname, pre, floder))
       } catch (error) {
         console.log(err, 'mkdirSync catch')
       }
     }
-    pre = path.join(pre, floder) // 路径拼合
+    pre = join(pre, floder) // 路径拼合
+  })
+}
+
+/**
+ * 重写文件
+ * @param {String} pathname
+ * @param {String} tempname
+ * @param {Function} format
+ */
+export const rewriteFile = ({ pathname, tempname, format = res => res }) => {
+  readFile(tempname, 'utf8', (err, res) => {
+    if (err) return console.log('读取文件失败', err.message)
+    writeFile(resolve(pathname), format(res), err => {
+      if (err) return console.log('生成文件失败', err.message)
+    })
   })
 }
